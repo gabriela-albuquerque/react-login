2b2325a2974369eda02318fc8f94e28de6a3d05c
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2025-07-25 18:53:38 -0300

    iniciado jsx do componente Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Login from "./Components/Login";
 
 function App() {
   return (
-    <div>
-      <p>Página de Login</p>
+    <div className="App">
+      <Login />
     </div>
   );
 }
