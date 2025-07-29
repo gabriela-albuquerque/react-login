e63c89a6be9266feec853055978a7658e712abf3
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2025-07-29 18:06:02 -0300

    finalizada estilizacao

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -18,18 +18,20 @@ const Login = () => {
     <div className="container">
       <form onSubmit={handleSubmit}>
         <h1>Acesse o sistema</h1>
-        <div>
+        <div className="input-field">
           <input
             type="email"
             placeholder="E-mail"
+            required
             onChange={(e) => setUsername(e.target.value)}
           />
           <FaUser className="icon" />
         </div>
-        <div>
+        <div className="input-field">
           <input
             type="password"
             placeholder="Senha"
+            required
             onChange={(e) => setPassword(e.target.value)}
           />
           <FaLock className="icon" />
