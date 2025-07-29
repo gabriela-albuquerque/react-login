b847d1827b1528f23cfa66acd44cc38f45f9824a
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2025-07-29 09:17:39 -0300

    dados dos inputs capturados e prontos para envio

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,16 +1,37 @@
 import { FaUser, FaLock } from "react-icons/fa";
 
+import { useState } from "react";
+
+import "./Login.css";
+
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    alert("Enviando os dados:" + username + "-" + password);
+  };
+
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Acesse o sistema</h1>
         <div>
-          <input type="email" placeholder="E-mail" />
+          <input
+            type="email"
+            placeholder="E-mail"
+            onChange={(e) => setUsername(e.target.value)}
+          />
           <FaUser className="icon" />
         </div>
         <div>
-          <input type="password" placeholder="Senha" />
+          <input
+            type="password"
+            placeholder="Senha"
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <FaLock className="icon" />
         </div>
 
