46cc44eb7bc480064c18ea27e5f31f8c14b23019
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2025-07-29 08:46:18 -0300

    concluido html do componente Login

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,3 +1,5 @@
+import { FaUser, FaLock } from "react-icons/fa";
+
 const Login = () => {
   return (
     <div className="container">
@@ -5,11 +7,27 @@ const Login = () => {
         <h1>Acesse o sistema</h1>
         <div>
           <input type="email" placeholder="E-mail" />
+          <FaUser className="icon" />
         </div>
         <div>
           <input type="password" placeholder="Senha" />
+          <FaLock className="icon" />
+        </div>
+
+        <div className="recall-forget">
+          <label>
+            <input type="checkbox" />
+            Lembrar de mim
+          </label>
+          <a href="#">Esqueci minha senha</a>
         </div>
         <button>Entrar</button>
+
+        <div className="signup-link">
+          <p>
+            Não tem uma conta? <a href="#">Registrar</a>
+          </p>
+        </div>
       </form>
     </div>
   );
